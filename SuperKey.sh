eb718d8f36d2fa9c94c527391f2748aeaef7ac81
speltriao <38846168+speltriao@users.noreply.github.com>
2021-01-23 22:22:16 -0300

    Update SuperKey.sh

--- SuperKey.sh
+++ SuperKey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 var=$(wmctrl -lix |grep 'Your_program') #check if program is opened in wmctrl and save to var
 [[ ! -z "$var" ]] &&  # check if var is not empty ( not empty = program is already running)
-	pidof Your_program >/dev/null && wmctrl -lix |grep 'Your_program'|cut -d ' ' -f 1 |xargs -i% wmctrl -i -a %  || #if is already running, select window
+	pidof your_program >/dev/null && wmctrl -lix |grep 'Your_program'|cut -d ' ' -f 1 |xargs -i% wmctrl -i -a %  || #if is already running, select window
 		your_program #if is not running, then run it!
